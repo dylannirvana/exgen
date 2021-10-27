97a840e97ff018a83d26b96f575044854db8bfed
Dylan Nirvana <dylan@dylannirvana.com>
2021-10-27 10:09:10 -0400

    adds auth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var wwwhisper = require('connect-wwwhisper');
+
 // var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 
@@ -23,6 +25,8 @@ var app = express();
 // app.use('/users', usersRouter);
 
 app.use(express.static('client/public'))
+app.use(wwwhisper());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
